cd6955ad2f096c5aaf271815ab38ee63b6b96814
MelaniaPicone <melania_picone@yahoo.it>
2025-07-14 21:18:05 +0200

    first snack

--- js/script.js
+++ js/script.js
@@ -3,27 +3,27 @@
 const bikes = [
 {
 nome: 'Giant TCR Advanced',
-peso: '6,4', 
+peso: '6.4', 
 },
 
 { 
 nome: 'Scott Addict RC',
-peso: '5,9', 
+peso: '5.9', 
 },
 
 { 
 nome: 'Aethos Stram Red',
-peso: '6,2', 
+peso: '6.2', 
 },
 
 { 
 nome: 'Wilier Triestina',
-peso: '6,8', 
+peso: '6.8', 
 },
 
 { 
 nome: 'Orbea Orca',
-peso: '7,2', 
+peso: '7.2', 
 },
 
 { 
@@ -33,10 +33,24 @@ peso: '7',
 
 { 
 nome: 'Shimano ACE',
-peso: '7,4', 
+peso: '7.4', 
 }
 ];
 
 console.log(bikes);
 
+let lightBike = bikes[0];
+
+// ciclo 
+for (let i = 0; i<bikes.length ; i++) {
+let kg = parseFloat(bikes[i].peso);
+let maxKg = parseFloat(lightBike.peso);
+
+
+if (kg < maxKg) {
+heavBike = bikes[i];
+console.log(lightBike);
+}
+
+}
 
