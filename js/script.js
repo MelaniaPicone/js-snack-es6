65c3eb3691886b68ebff2913fff80c2037b40b20
MelaniaPicone <melania_picone@yahoo.it>
2025-07-15 21:02:34 +0200

    [fix] bikes peso

--- js/script.js
+++ js/script.js
@@ -3,37 +3,37 @@
 const bikes = [
 {
 nome: 'Giant TCR Advanced',
-peso: '6.4', 
+peso: 6.4, 
 },
 
 { 
 nome: 'Scott Addict RC',
-peso: '5.9', 
+peso: 5.9, 
 },
 
 { 
 nome: 'Aethos Stram Red',
-peso: '6.2', 
+peso: 6.2, 
 },
 
 { 
 nome: 'Wilier Triestina',
-peso: '6.8', 
+peso: 6.8, 
 },
 
 { 
 nome: 'Orbea Orca',
-peso: '7.2', 
+peso: 7.2, 
 },
 
 { 
 nome: 'Look Blade RS',
-peso: '7', 
+peso: 7, 
 },
 
 { 
 nome: 'Shimano ACE',
-peso: '7.4', 
+peso: 7.4, 
 }
 ];
 
@@ -43,8 +43,8 @@ let lightBike = bikes[0];
 
 // ciclo 
 for (let i = 0; i<bikes.length ; i++) {
-let kg = parseFloat(bikes[i].peso);
-let maxKg = parseFloat(lightBike.peso);
+let kg = bikes[i].peso;
+let maxKg = lightBike.peso;
 
 
 if (kg < maxKg) {
