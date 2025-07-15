1f6bb3625ace40b59c77fc3464c3df45226f0e61
MelaniaPicone <melania_picone@yahoo.it>
2025-07-15 21:11:40 +0200

    [add] random squad points

--- js/scripttwo.js
+++ js/scripttwo.js
@@ -2,37 +2,55 @@ const squad = [
 
 {
 nome: 'Juventus',
-punti_assegnati: '0',
-falli_subiti: '0',
+punti_assegnati: 0,
+falli_subiti: 0,
 },
 
 
 {
 nome: 'Inter',
-punti_assegnati: '0',
-falli_subiti: '0',
+punti_assegnati: 0,
+falli_subiti: 0,
 },
 
 
 {
 nome: 'Napoli',
-punti_assegnati: '0',
-falli_subiti: '0',
+punti_assegnati: 0,
+falli_subiti: 0,
 },
 
 
 {
 nome: 'Fiorentina',
-punti_assegnati: '0',
-falli_subiti: '0',
+punti_assegnati: 0,
+falli_subiti: 0,
 },
 
 
 {
 nome: 'Roma',
-punti_assegnati: '0',
-falli_subiti: '0',
+punti_assegnati: 0,
+falli_subiti: 0,
 },
 ]
 
 console.log(squad);
+
+
+// numeri randomici
+
+function randomNumb () {
+  randomNumb = Math.floor(Math.random() * 100 +1);
+return randomNumb;
+}
+
+
+// aggiunta numeri randomici a punti e falli
+
+for (let i=0; i < squad.length ; i++) {
+
+
+
+  
+}
